65a61607e58c370724c3ac0c9251f3de301b5d66
Android Studio <devandroid@ort>
2025-08-14 11:35:35 -0300

    Modificando archivo inputCity.js

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -1,12 +1,11 @@
 // src/components/WeatherApp.jsx
 
 import React, { useEffect, useState } from 'react';
-import { useCity } from '../context/CityContext';
 import { fetchWeatherData } from '../services/weather-app-service';
+import {  useResultado } from 'src/context/resultadoConsultaContext.jsx';
 
-const WeatherApp = () => {
-  const { city } = useCity();
-  const [weatherData, setWeatherData] = useState(null);
+export const WeatherApp = () => {
+  const {setWeatherData} = useResultado();
   const [error, setError] = useState('');
 
   useEffect(() => {
